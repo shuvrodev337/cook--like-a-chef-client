6e638b30c6a884bb09f2a6f77b188a3e177b4cfb
unknown <shuvrodevmondal337@gmail.com>
2023-05-02 19:36:18 +0600

    added footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className='footer mt-auto py-3 bg-dark'>
+            <div className='container'>
+            <h2 className="text-white text-center">This is footer</h2>
+        </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import NavigationBar from '../components/NavigationBar/NavigationBar';
 import { Outlet } from 'react-router-dom';
+import Footer from '../components/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
             <NavigationBar></NavigationBar>
+            <div className='container'>
             <Outlet></Outlet>
+            </div>
+            <div className='d-flex flex-column min-vh-100'>
+            <Footer></Footer>
+            </div>
         </div>
     );
 };
