71abffd3bb5df32024c1fc7cb80336ac0b6bc895
unknown <shuvrodevmondal337@gmail.com>
2023-05-03 02:44:56 +0600

    added tooltip on user image

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,10 +1,11 @@
 import React, { useContext } from "react";
-import { Button, Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import { Button, Container, Image, Nav, NavDropdown, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../providers/AuthProviders";
-
+import { Tooltip } from 'react-tooltip'
 const NavigationBar = () => {
   const {user,logOut} = useContext(AuthContext)
+  // console.log(user.photoURL);
   const handleLogOut = () =>{
     logOut()
     alert("Log Out successfull")
@@ -12,7 +13,7 @@ const NavigationBar = () => {
   }
   return (
    
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="white">
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Link className="fs-3 fw-bolder text-decoration-none"><span className="text-white">Cook</span><span className="text-danger">Like-a-</span><span className="text-white">Chef</span></Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -27,7 +28,11 @@ const NavigationBar = () => {
           <Nav>
           {
             user ?
-            <Link ><Button onClick={handleLogOut} className="text-white fw-semibold" variant="warning">Log Out</Button>{' '}</Link> :
+            <>
+            <Tooltip className="bg-danger" id="my-tooltip" />
+            <Image data-tooltip-id="my-tooltip" data-tooltip-content={user?.displayName} className="me-3" src={user?.photoURL} style={{width:"36px",height:"36px"}} roundedCircle />
+            <Button onClick={handleLogOut} className="text-white fw-semibold" variant="warning">Log Out</Button>{' '}
+            </> :
             <Link to={"/login"}><Button className="text-white fw-semibold" variant="danger">Login</Button>{' '}</Link>
           }
            
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-tooltip/dist/react-tooltip.css'
 import {
   createBrowserRouter,
   RouterProvider,
