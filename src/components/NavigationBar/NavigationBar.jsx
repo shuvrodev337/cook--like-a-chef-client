e467f87e69e3e5a5504e857c0a02cf402e2d71b3
unknown <shuvrodevmondal337@gmail.com>
2023-05-02 17:25:23 +0600

    updated navbar

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -9,14 +9,13 @@ const NavigationBar = () => {
     //     <Link to={"/login"}>Login</Link>
     //     <Link to={"/register"}>Register</Link>
     // </div>
-    <Navbar collapseOnSelect expand="lg" bg="danger" variant="danger">
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="white">
       <Container>
-        <Link className="fs-3 fw-bolder text-decoration-none"><span className="text-warning">Cook</span><span className="text-white">Like-a-</span><span className="text-warning">Chef</span></Link>
+        <Link className="fs-3 fw-bolder text-decoration-none"><span className="text-white">Cook</span><span className="text-danger">Like-a-</span><span className="text-white">Chef</span></Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto me-3">
-            {/* <Nav.Link href="#features">Home</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link> */}
+            
             <Link className="text-white me-2  fs-5 fw-semibold  text-decoration-none" to={"/"}>Home</Link>
             <Link className="text-white  fs-5 fw-semibold  text-decoration-none" to={"/blog"}>Blog</Link>
 
@@ -24,7 +23,7 @@ const NavigationBar = () => {
           </Nav>
           <Nav>
           
-            <Link to={"/login"}><Button className="text-white " variant="warning">Login</Button>{' '}</Link>
+            <Link to={"/login"}><Button className="text-white fw-semibold" variant="danger">Login</Button>{' '}</Link>
            
           </Nav>
         </Navbar.Collapse>
