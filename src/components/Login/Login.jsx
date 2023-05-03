1e9bccb63a702d447a63fd217bba2a71ff50ff24
unknown <shuvrodevmondal337@gmail.com>
2023-05-03 16:54:35 +0600

    added email password error

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -6,10 +6,10 @@ import { AuthContext } from "../../providers/AuthProviders";
 const Login = () => {
   // const [successMessage, setSuccessMessage] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
-  const {logIn, googleSignIn, githubSighnIn} = useContext(AuthContext)
+  const { logIn, googleSignIn, githubSighnIn } = useContext(AuthContext);
   const navigate = useNavigate();
   const location = useLocation();
-  const from = location.state?.from?.pathname || '/'
+  const from = location.state?.from?.pathname || "/";
 
   const handleEmailPasswordLogin = (event) => {
     event.preventDefault();
@@ -17,48 +17,51 @@ const Login = () => {
     const email = form.email.value;
     const password = form.password.value;
     console.log(email, password);
-    setErrorMessage("")
-    logIn(email,password)
-    .then(result=>{
-      const loggedUser = result.user
-      console.log(loggedUser);
-      form.reset();
-      navigate(from);
-    })
-    .catch((error) => {
-      setErrorMessage(error.message);
-    });
+    setErrorMessage("");
+    logIn(email, password)
+      .then((result) => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        form.reset();
+        navigate(from);
+      })
+      .catch((error) => {
+        if (error.message === "Firebase: Error (auth/user-not-found).") {
+          setErrorMessage("The email address you entered isn't connected to an account!");
+        } else if (error.message === "Firebase: Error (auth/wrong-password).") {
+          setErrorMessage("The password that you've entered is incorrect!");
+        }
+        console.log(error.message);
+      });
   };
   //==========//
   const handleResetPassword = (event) => {};
-//===========//
+  //===========//
   const handleGoogleSignIn = () => {
     googleSignIn()
-    .then(result =>{
-      const googleLoggedUser = result.user
-      console.log(googleLoggedUser);
-      navigate(from);
-    })
-    .catch((error) => {
-      setErrorMessage(error.message);
-    });
+      .then((result) => {
+        const googleLoggedUser = result.user;
+        console.log(googleLoggedUser);
+        navigate(from);
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+      });
   };
   const handleGithubSignIn = () => {
     githubSighnIn()
-    .then(result =>{
-      const githubLoggedUser = result.user
-      console.log(githubLoggedUser);
-      navigate(from);
-    })
-    .catch((error) => {
-      setErrorMessage(error.message);
-    });
+      .then((result) => {
+        const githubLoggedUser = result.user;
+        console.log(githubLoggedUser);
+        navigate(from);
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+      });
   };
   return (
     <div className="w-50 mx-auto mt-5 bg-light p-4 rounded shadow">
-      <h2 className="text-center fw-bold fs-1 text-primary my-5">
-        Login
-      </h2>
+      <h2 className="text-center fw-bold fs-1 text-primary my-5">Login</h2>
       <form onSubmit={handleEmailPasswordLogin}>
         <Form.Group className="mb-3" controlId="email">
           <Form.Label className="text-primary">Email </Form.Label>
@@ -79,18 +82,23 @@ const Login = () => {
             required
           />
         </Form.Group>
-          <p>
-          <small>
-            Forgot password?{" "}
-          </small>
-            <button onClick={handleResetPassword} className="btn btn-link text-decoration-none">
-              Reset password
-            </button>
+        <p>
+          <small>Forgot password? </small>
+          <button
+            onClick={handleResetPassword}
+            className="btn btn-link text-decoration-none"
+          >
+            Reset password
+          </button>
         </p>
         {/* <p className="text-success">{successMessage}</p> */}
         <p className="text-danger">{errorMessage}</p>
         <div className="text-center my-3">
-          <Button className="w-50 fw-semibold fs-4" variant="primary" type="submit">
+          <Button
+            className="w-50 fw-semibold fs-4"
+            variant="primary"
+            type="submit"
+          >
             Login
           </Button>
         </div>
@@ -98,13 +106,20 @@ const Login = () => {
 
       <div className="d-flex justify-content-center gap-3">
         <div className="mt-2">
-          <Button onClick={handleGoogleSignIn} variant="outline-danger" type="submit">
+          <Button
+            onClick={handleGoogleSignIn}
+            variant="outline-danger"
+            type="submit"
+          >
             Sign In with Google
           </Button>
-          
         </div>
         <div className="mt-2">
-          <Button onClick={handleGithubSignIn} variant="outline-secondary" type="submit">
+          <Button
+            onClick={handleGithubSignIn}
+            variant="outline-secondary"
+            type="submit"
+          >
             Sign In with Github
           </Button>
         </div>
@@ -113,10 +128,9 @@ const Login = () => {
         <p className="text-success">
           New to CookLike-a-Chef ?{" "}
           <Link className="text-decoration-none" to={"/register"}>
-            <small >Sign up or Register</small>
+            <small>Sign up or Register</small>
           </Link>
         </p>
-        
       </div>
     </div>
   );
